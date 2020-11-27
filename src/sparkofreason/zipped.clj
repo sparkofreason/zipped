8c9cb0a313a3d8fbb0a478f1ab9606ff6fa51f04
Dave Dixon <dave@pop-os.localdomain>
2020-11-27 14:29:41 -0800

    Pull in storm debugger

--- src/sparkofreason/zipped.clj
+++ src/sparkofreason/zipped.clj
@@ -6,9 +6,12 @@
             [rewrite-cljc.reader :as r])
   (:gen-class))
 
-(defrecord ErrorNode [message string-value])
+(clojure.pprint/pprint (p/parse-string "@a"))
+(defrecord IncompleteNode [message string-value])
+(defrecord IncompleteDerefNode [message string-value])
+(defrecord IncompleteKeywordNode [message string-value])
 
-(def empty-expr #sparkofreason.zipped.ErrorNode["", ""])
+(def empty-expr #sparkofreason.zipped.IncompleteNode["", ""])
 
 (def enclosure-matches
   {\" \"
@@ -21,27 +24,37 @@
   (cond
     (re-find #".*EOF.*" msg) :eof
     (re-find #".*Invalid keyword.*" msg) :keyword
+    (re-find #".*:deref.*" msg) :deref
     :else :unknown))
 
 (defmulti handle-keypress
   (fn [node k] (type node)))
 
-(defmethod handle-keypress ErrorNode
+(defmethod handle-keypress IncompleteNode
   [node k]
   (let [string-value (:string-value node)]
     (let [string-value' (str string-value k)]
       (try
-        (p/parse-string string-value')
+        #trace (p/parse-string string-value')
         (catch clojure.lang.ExceptionInfo ex
           (let [message (.getMessage ^Throwable ex)
                 error-type (error-message->type message)]
             (println string-value' error-type message)
             (case error-type
               :eof (if-let [match (enclosure-matches k)]
-                     (handle-keypress (->ErrorNode message string-value') match)
-                     (->ErrorNode message string-value'))
-              :keyword (->ErrorNode message string-value')
+                     (handle-keypress (->IncompleteNode message string-value') match)
+                     (->IncompleteNode message string-value'))
+              :keyword (->IncompleteKeywordNode message string-value')
+              :deref (->IncompleteDerefNode message string-value')
               node)))))))
+
+(defmethod handle-keypress IncompleteDerefNode
+  [node k]
+  (let [{:keys [string-value]} node]
+    (try
+      (p/parse-string (str string-value k))
+      (catch clojure.lang.ExceptionInfo ex
+        node))))
 
 (defmethod handle-keypress rewrite_cljc.node.token.TokenNode
   [node k]
@@ -69,7 +82,7 @@
       (= \/ k)
       (if (or namespaced? has-namespace?)
         node
-        (handle-keypress (->ErrorNode "" string-value) k))
+        (handle-keypress (->IncompleteNode "" string-value) k))
       
       :else
       (try
@@ -77,52 +90,11 @@
         (catch clojure.lang.ExceptionInfo ex
           node)))))
 
-(def reader-dispatch (handle-keypress empty-expr "#"))
 (comment 
- (handle-keypress empty-expr \()
- (handle-keypress empty-expr \[)
- (handle-keypress empty-expr \{)
- (handle-keypress empty-expr \")
- (handle-keypress empty-expr \a)
  
- (handle-keypress empty-expr \:)
- (-> empty-expr
-     (handle-keypress \:)
-     (handle-keypress \a))
- 
- (-> empty-expr
-     (handle-keypress \:)
-     (handle-keypress \a)
-     (handle-keypress \/))
- (-> empty-expr
-     (handle-keypress \:)
-     (handle-keypress \a)
-     (handle-keypress \/)
-     (handle-keypress \b))
- (-> empty-expr
-     (handle-keypress \:)
-     (handle-keypress \a)
-     (handle-keypress \/)
-     (handle-keypress \b)
-     (handle-keypress \/))  
- 
- (-> empty-expr
-     (handle-keypress \a)
-     (handle-keypress \b)
-     (handle-keypress \())
-  
- (-> empty-expr
-     (handle-keypress \a)
-     (handle-keypress \,))
-  
- (handle-keypress reader-dispatch \()
- (handle-keypress reader-dispatch \[)
- (handle-keypress reader-dispatch \{)
- (handle-keypress reader-dispatch \")
-
  (r/whitespace-or-boundary? \/)
  
- (p/parse-string "#bar[foo]")
+ (p/parse-string "@")
  (p/parse-string "#(")
  (p/parse-string "(")
  (p/parse-string "foo")
--- test/sparkofreason/zipped+test.clj
+++ test/sparkofreason/zipped+test.clj
@@ -2,6 +2,65 @@
   (:require [clojure.test :refer :all]
             [sparkofreason.zipped :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(require '[flow-storm.api :as fsa])
+(fsa/connect)
+
+(def reader-dispatch (handle-keypress empty-expr "#"))
+
+(handle-keypress empty-expr \()
+(handle-keypress empty-expr \[)
+(handle-keypress empty-expr \{)
+(handle-keypress empty-expr \")
+(handle-keypress empty-expr \a)
+
+(handle-keypress empty-expr \:)
+(-> empty-expr
+  (handle-keypress \:)
+  (handle-keypress \a))
+
+(-> empty-expr
+  (handle-keypress \:)
+  (handle-keypress \a)
+  (handle-keypress \/))
+(-> empty-expr
+  (handle-keypress \:)
+  (handle-keypress \a)
+  (handle-keypress \/)
+  (handle-keypress \b))
+(-> empty-expr
+  (handle-keypress \:)
+  (handle-keypress \a)
+  (handle-keypress \/)
+  (handle-keypress \b)
+  (handle-keypress \/))  
+
+(-> empty-expr
+  (handle-keypress \a)
+  (handle-keypress \b)
+  (handle-keypress \())
+
+(-> empty-expr
+  (handle-keypress \a)
+  (handle-keypress \,))
+
+(-> empty-expr 
+    (handle-keypress \@)
+    (handle-keypress \a))
+
+(-> empty-expr 
+    (handle-keypress \@)
+    (handle-keypress \a))
+
+#trace
+(-> empty-expr 
+    (handle-keypress \@)
+    (handle-keypress \[)
+    (handle-keypress \a)
+    (.toString))
+
+
+(handle-keypress empty-expr \])
+(handle-keypress reader-dispatch \()
+(handle-keypress reader-dispatch \[)
+(handle-keypress reader-dispatch \{)
+(handle-keypress reader-dispatch \")
